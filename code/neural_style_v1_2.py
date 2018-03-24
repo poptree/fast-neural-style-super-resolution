e51a54c45010ec5ba4ab232bc6f5d3e5c188b81b
poptree <poptreexie@gmail.com>
2018-03-24 13:22:35 +0800

    3.24

--- code/neural_style_v1_2.py
+++ code/neural_style_v1_2.py
@@ -29,10 +29,14 @@ def train(args):
     train_loader_LR, train_loader_HR, dataset_len = utils.make_dataset(args)
 
     transformer = utils.make_model(args)
+    if args.updata:
+        transformer.load_state_dict(torch.load(args.modeldir))
     vgg = utils.make_vggmodel(args)
 
     optimizer = Adam(transformer.parameters(), lr=args.lr)
     mse_loss = torch.nn.MSELoss()
+    if args.cuda:
+        mse_loss.cuda()
 
     for e in range(args.epochs):
         log_msg = "pix_weight = "+str(args.pix_weight)+"   content_weight = "+str(args.content_weight)
@@ -49,9 +53,10 @@ def train(args):
             optimizer.zero_grad()
 
             pix_x_v = Variable(x)
-            pix_s_v = Variable(style)
+            pix_s_v = Variable(style,requires_grad=False)
 
             pix_loss = 0
+            content_loss = 0
 
             if args.cuda:
                 x=x.cuda()
@@ -61,24 +66,46 @@ def train(args):
 
             output = transformer(pix_x_v)
             pix_loss = args.pix_weight * mse_loss(output, pix_s_v)
-            pix_loss.backward()
+
+            vgg_s = Variable(style.clone(),requires_grad=False)
+
+            vgg_x = utils.init_vgg_input(output)
+            vgg_s = utils.init_vgg_input(vgg_s)
+
+
+            feature_x = vgg(vgg_x)
+            feature_s = vgg(vgg_s)
+
+            f_s_v = Variable(feature_s[1].data, requires_grad=False)
+
+            content_loss = args.content_weight * mse_loss(feature_x[1], f_s_v)
+
+            total_loss = 0
+            if args.pix_weight>0:
+                total_loss+=pix_loss
+            if args.content_weight>0:
+                total_loss+=content_loss
+
+            total_loss.backward()
             optimizer.step()
+            agg_content_loss += content_loss.data[0]
             agg_pix_loss += pix_loss.data[0]
+            up = 10000
 
             if(batch_id + 1) % args.log_interval == 0:
                 mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tpix: {:.6f}\ttotal: {:.6f}".format(\
                 time.ctime(), e + 1, count, dataset_len,\
-                agg_content_loss / (batch_id + 1),\
-                agg_pix_loss / (batch_id + 1),\
-                (agg_content_loss + agg_pix_loss) / (batch_id + 1))
-                print(mesg)       
+                agg_content_loss*up / (batch_id + 1),\
+                agg_pix_loss *up/ (batch_id + 1),\
+                (agg_content_loss + agg_pix_loss)*up / (batch_id + 1))
+                print(mesg)         
 
 
     transformer.eval()
     transformer.cpu()
     save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
     args.content_weight) + "_SRCNN_" + str(args.srcnn) + ".model"
-    save_model_path = os.path.join(args.save_model_dir, save_model_filename)
+    save_model_path = "mymodel.model"
     torch.save(transformer.state_dict(), save_model_path)
 
     print("\nDone, trained model saved at", save_model_path)    
@@ -87,13 +114,14 @@ def train(args):
 
 
 def stylize(args):
-    content_img = utils.load_image_to_tensor(args.content_image,args.cuda)
-    content_img.unsqueeze_(0)
+    content_image = utils.load_image_to_tensor(args.content_image,args.cuda)
+    content_image.unsqueeze_(0)
+    content_image = Variable(content_image)
 
     model = utils.make_model(args)
     model.load_state_dict(torch.load(args.model))
 
-    output_image = model(content_img)
+    output_image = model(content_image)
     output_image = output_image.data
     output_image.squeeze_(0)
     utils.save_tensor_to_image(output_image,args.output_image,args.cuda)
@@ -111,7 +139,7 @@ def main():
     train_arg_parser.add_argument("--dataset", type=str, required=True,help="path to training dataset, the path should point to a folder "+"containing another folder with all the training images")
     train_arg_parser.add_argument("--upsample", type=int, default=4, required=True,help="the factor of the upsample 4 or 8")
     train_arg_parser.add_argument("--save-model-dir", type=str, required=True,help="path to folder where trained model will be saved.")
-    train_arg_parser.add_argument("--image-size", type=int, default=256,help="size of training images, default is 256 X 256")
+    train_arg_parser.add_argument("--image-size", type=int, default=500,help="size of training images, default is 256 X 256")
     train_arg_parser.add_argument("--style-size", type=int, default=None,help="size of style-image, default is the original size of style image")
     train_arg_parser.add_argument("--cuda", type=int, required=True, help="set it to 1 for running on GPU, 0 for CPU")
     train_arg_parser.add_argument("--seed", type=int, default=42, help="random seed for training")
@@ -120,6 +148,9 @@ def main():
     train_arg_parser.add_argument("--lr", type=float, default=1e-3,help="learning rate, default is 0.001")
     train_arg_parser.add_argument("--log-interval", type=int, default=500,help="number of images after which the training loss is logged, default is 500")
     train_arg_parser.add_argument("--srcnn", type=int, default=0,help="set it to 1 for running on srcnn")
+    train_arg_parser.add_argument("--updata", type=int, default=0,help="set 1 to updata model")
+    train_arg_parser.add_argument("--modeldir", type=str, default=0,help="set 1 to updata model")
+
     
     
     eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
